658edb840367c0cb8eaf4179b16142a8971cf388
imrod22 <46498650+imrod22@users.noreply.github.com>
2025-02-10 20:46:34 -0300

    added configurations

--- messi_by_x/messi_by_x/src/main/java/fetch_information/messi_x_tweets/TweetAnalysis.java
+++ messi_by_x/messi_by_x/src/main/java/fetch_information/messi_x_tweets/TweetAnalysis.java
@@ -14,6 +14,8 @@ public class TweetAnalysis {
 		
 		Configuration conf = new Configuration();
 
+		//PRUEBAS 2 y 3
+
 		//conf.set("mapreduce.map.memory.mb", "2048");
 		//conf.set("mapreduce.reduce.memory.mb", "3072");
 
@@ -24,7 +26,10 @@ public class TweetAnalysis {
         //conf.setInt("mapreduce.shuffle.memory.limit", 1024);
 
 		//conf.set("mapreduce.input.fileinputformat.split.maxsize", "16777216");
-		//conf.set("mapreduce.input.fileinputformat.split.minsize", "8388608"); 
+		//conf.set("mapreduce.input.fileinputformat.split.minsize", "8388608");
+
+		//conf.set("mapreduce.input.fileinputformat.split.maxsize", "33554432");
+		//conf.set("mapreduce.input.fileinputformat.split.minsize", "16777216");
 
 		//conf.set("mapreduce.task.io.sort.mb", "128");
 		//conf.set("mapreduce.map.sort.spill.percent", "0.8");
@@ -37,9 +42,30 @@ public class TweetAnalysis {
 		//conf.set("mapreduce.reduce.shuffle.input.buffer.percent", "0.5");
 		//conf.set("mapreduce.reduce.shuffle.merge.percent", "0.4");
 		//conf.set("mapreduce.reduce.shuffle.parallelcopies", "10");
+
+		//PRUEBA 4
+
+		//conf.set("mapreduce.map.memory.mb", "2048");
+		//conf.set("mapreduce.reduce.memory.mb", "3072");
+//
+		//conf.set("mapreduce.map.java.opts", "-Xmx1800m");
+		//conf.set("mapreduce.reduce.java.opts", "-Xmx2560m");
+//
+        //conf.setInt("mapreduce.shuffle.parallelcopies", 2);
+        //conf.setInt("mapreduce.shuffle.memory.limit", 1024);
+//
+		//conf.set("mapreduce.input.fileinputformat.split.maxsize", "33554432");		
+//
+		//conf.set("mapreduce.reduce.shuffle.input.buffer.percent", "0.7");
+		//conf.set("mapreduce.reduce.shuffle.merge.percent", "0.5");
+//
+		//conf.setInt("mapreduce.shuffle.parallelcopies", 20);
+		//conf.setInt("mapreduce.reduce.shuffle.parallelcopies", 20);
+//
+		//conf.set("mapreduce.job.reduces", "2");
 	     
 	    Job job = Job.getInstance(conf, "Messi en Red Social X");
-		
+		//
 		job.addFileToClassPath(new Path("/libs/javavader/vader-sentiment-analyzer-1.0.jar"));
 		job.addFileToClassPath(new Path("/libs/javavader/commons-lang3-3.12.0.jar"));
 		job.addFileToClassPath(new Path("/libs/javavader/opennlp-tools-2.0.0.jar"));
